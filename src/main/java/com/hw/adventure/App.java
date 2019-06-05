17d853108f26605178ee1d013d9e8092e7d19301
hwang777 <hw2@hotmail.com>
2019-06-04 23:23:25 -0700

    Changed file structure

---
+++ src/main/java/com/hw/adventure/App.java
@@ -0,0 +1,19 @@
+package com.hw.adventure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private int i = 0;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int getI()
+    {
+    	return i;
+    }
+
+}
